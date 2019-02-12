2a9d4fd39dbc13020672db496a85fc64f16896d6
CzA11 <cande113@mail.depaul.edu>
2019-02-11 21:14:17 -0500

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -5,21 +5,20 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(2)
 
 
-port = 1000
-for portx in range(1, 100):
+###port = 65535
+for portx in range(1, 65536):
     try:
-        s.connect(('ad.samsclass.info', port))
-        r = s.receive(1024)
+        s.connect(('ad.samsclass.info', portx))
+        r = s.recv(1024)
         #When connected to port, sample server should return Congratulations
         if 'Congratulations' in r.decode('utf8'):
             print('[!] HIDDEN SERVICE FOUND: %s ~ %s' % (port, r.decode('utf8')))
-            break
             s.close()
+            break
         else:
-            print('%s ~ %s' % (port, r.decode('utf8'))
-            #s.close()
-    except socket.error as e:
-        print('%s ~ %s' % (port, err))
+            print('%s ~ %s' % (portx, r.decode('utf8')))
+            s.close()
+    except socket.error as err:
+        print('%s ~ %s' % (portx, err))
 
-    port += 1000
-                  
+    port += 1000   
